3fee8d518b0367582a5f3d4a3e81c374c27e928c
alexander <alexander_199@hotmail.se>
2025-12-18 09:48:32 +0100

    signup controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
 
 export default app;
